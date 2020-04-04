c972799d293ddf805dc80f80be2ea36073681073
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-04-04 14:32:52 +0700

    update port heroku

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8888
 const duongDan = require('path')
 
 app.use('/open', express.static(duongDan.join(__dirname, 'public')))
