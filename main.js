836c1dc5049a2805820ad3b901b4933e00ee3a5b
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-04-04 16:55:58 +0700

    update code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/chat.html');
